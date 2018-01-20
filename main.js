cd5540a66181734cccb58a4ce1e4a04960f309dc
zoey <echohyt@sohu.com>
2018-01-20 15:14:30 +0800

    增加了自动轮播效果，且点击后重新开始轮播

--- main.js
+++ main.js
@@ -12,18 +12,50 @@ $slide.each(function(i){
 
 $(`#slides`).width(totWidth);
 
-goToSlide()
+$(`#menu ul li.menuItem:first`).addClass(`act`).siblings().addClass(`inact`);
 
-function goToSlide(){ 
-    $menus.on(`click`, function(e){
-    $(`li.menuItem`).removeClass(`act`).addClass(`inact`);
-    $(this).parent().addClass(`act`);
-    var pos = $(this).parent().prevAll(`.menuItem`);
-    $(`#slides`).stop().animate({marginLeft:-positions[pos.length]+`px`},450);
-    e.preventDefault();
-    });
-    $(`#menu ul li.menuItem:first`).addClass(`act`).siblings().addClass(`inact`);
+
+//点击切换
+let $menuItems = $(`li.menuItem`)
+let $menuItemsArray = Array.from($menuItems)
+for (let i = 0; i < $menuItems.length; i++) {   
+       $($menuItems[i]).on('click', function(e) {
+        $menuItems.removeClass(`act`).addClass(`inact`);
+        $(this).addClass(`act`);
+        var pos = $(this).prevAll(`.menuItem`);
+        iNow = pos.length
+        $('#slides').css({transform: `translateX(`+ (-positions[pos.length]) + 'px)'})
+        e.preventDefault();
+    })    
 }
+
+//点击切换方法2
+// goToSlide()
+
+// function goToSlide(){ 
+//     $menus.on(`click`, function(e){
+//         $menuItems.removeClass(`act`).addClass(`inact`);
+//         $(this).parent().addClass(`act`);
+//         var pos = $(this).parent().prevAll(`.menuItem`);
+//         $(`#slides`).stop().animate({marginLeft:-positions[pos.length]+`px`},450);
+//         e.preventDefault();
+//     });
+// }
+
+
+//自动轮播
+var timer = null; 
+var iNow = 0;  
+timer = setInterval(function(){  
+    iNow++;       
+    if(iNow>$menuItems.length-1){ 
+        iNow=0;
+    }
+    $menuItems.eq(iNow).trigger("click"); 
+},3000);
+
+
+
 
 
 
